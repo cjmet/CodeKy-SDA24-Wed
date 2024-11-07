0ea046d563aba4e2c9ebb687464f04ad6d006781
cjmet <khaaiev@gmail.com>
2024-11-07 16:09:26 -0500

    first commit

---
+++ WeatherStation/Program.cs
@@ -0,0 +1,19 @@
+﻿using static AngelHornetLibrary.CLI.CliSystem;
+
+namespace WeatherStation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // WARNING: FindFileInVisualStudio is only intended for use while debugging COMMAND LINE only apps inside the Visual Studio IDE.
+            String? ghcndStationsFileName = FindFileInVisualStudio("ghcnd-stations.txt");
+            Console.WriteLine("ghcnd-stations.txt: " + ghcndStationsFileName);
+
+            String? ghcndStatesFileName = FindFileInVisualStudio("ghcnd-states.txt");
+            Console.WriteLine("ghcnd-states.txt: " + ghcndStatesFileName);
+
+            // add code here ... 
+        }
+    }
+}
