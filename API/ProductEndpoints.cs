70a826e379e32c737db7840f5c868a9c6cd8aea3
cjmet <khaaiev@gmail.com>
2025-02-19 20:19:31 -0500

    In-Class Update adding generic API Skeleton

---
+++ API/ProductEndpoints.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Data;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.OpenApi;
+using System.Diagnostics;
+
+namespace API;
+
+
+public static class ProductEndpoints
+{
+    public static void MapProductEndpoints (this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup("/api/Product").WithTags(nameof(Product));
+
+        group.MapGet("/", async (SQLiteContext db) =>
+        {
+            return await db.Products.ToListAsync();
+        })
+        .WithName("GetAllProducts")
+        .WithOpenApi();
+
+        group.MapGet("/{id}", async Task<Results<Ok<Product>, NotFound>> (int id, SQLiteContext db) =>
+        {
+            return await db.Products.AsNoTracking()
+                .FirstOrDefaultAsync(model => model.Id == id)
+                is Product model
+                    ? TypedResults.Ok(model)
+                    : TypedResults.NotFound();
+        })
+        .WithName("GetProductById")
+        .WithOpenApi();
+
+        group.MapPut("/{id}", async Task<Results<Ok, NotFound>> (int id, Product product, SQLiteContext db) =>
+        {
+            var affected = await db.Products
+                .Where(model => model.Id == id)
+                .ExecuteUpdateAsync(setters => setters
+                    .SetProperty(m => m.Id, product.Id)
+                    .SetProperty(m => m.isSelected, product.isSelected)
+                    .SetProperty(m => m.Brand, product.Brand)
+                    .SetProperty(m => m.Name, product.Name)
+                    .SetProperty(m => m.Description, product.Description)
+                    .SetProperty(m => m.Price, product.Price)
+                    .SetProperty(m => m.Quantity, product.Quantity)
+                    );
+            return affected == 1 ? TypedResults.Ok() : TypedResults.NotFound();
+        })
+        .WithName("UpdateProduct")
+        .WithOpenApi();
+
+        group.MapPost("/", async (Product product, SQLiteContext db) =>
+        {
+            db.Products.Add(product);
+            await db.SaveChangesAsync();
+            return TypedResults.Created($"/api/Product/{product.Id}",product);
+        })
+        .WithName("CreateProduct")
+        .WithOpenApi();
+
+        group.MapDelete("/{id}", async Task<Results<Ok, NotFound>> (int id, SQLiteContext db) =>
+        {
+            var affected = await db.Products
+                .Where(model => model.Id == id)
+                .ExecuteDeleteAsync();
+            return affected == 1 ? TypedResults.Ok() : TypedResults.NotFound();
+        })
+        .WithName("DeleteProduct")
+        .WithOpenApi();
+
+        group.MapPost("/init", async Task<Results<Ok,InternalServerError>> (SQLiteContext db) =>
+        {
+            db.Database.EnsureDeleted();
+            var results = db.Database.EnsureCreated();
+            Debug.WriteLine($"Database created: {results}");
+            Debug.WriteLine($"Database path: {db.DbPathName}");
+            return results  ? TypedResults.Ok() : TypedResults.InternalServerError();
+        })
+    .WithName("InitDatabase")
+    .WithOpenApi();
+    }
+}
